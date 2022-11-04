073c7444441f547e0b374f1307746959ef0c0304
Christer Edvartsen <christer.edvartsen@nav.no>
2022-11-04 09:13:09 +0100

    Update sgnatures and catch exceptions

--- src/SessionHandler.php
+++ src/SessionHandler.php
@@ -2,6 +2,7 @@
 namespace Naisdevice\Jita;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
 use SessionHandlerInterface;
 
 class SessionHandler implements SessionHandlerInterface
@@ -13,44 +14,31 @@ class SessionHandler implements SessionHandlerInterface
         $this->connection = $connection;
     }
 
-    /**
-     * Initialize session
-     *
-     * @param string $savePath
-     * @param string $sessionName
-     * @return true
-     */
-    public function open($savePath, $sessionName): bool
+    public function open(string $savePath, string $sessionName): bool
     {
         return true;
     }
 
-    /**
-     * Close the session
-     *
-     * @return bool
-     */
     public function close(): bool
     {
         return true;
     }
 
-    /**
-     * Read session data
-     *
-     * @param string $sessionId
-     * @return string|false
-     */
-    public function read($sessionId)
+    public function read(string $sessionId): string|false
     {
-        /** @var array{data:string,last_activity:int}|false */
-        $session = $this->connection->fetchAssociative(<<<SQL
-            SELECT data, last_activity
-            FROM sessions
-            WHERE id = :id
-        SQL, [
-            'id' => $sessionId,
-        ]);
+        try {
+            /** @var array{data:string,last_activity:int}|false */
+            $session = $this->connection->fetchAssociative(<<<SQL
+                SELECT data, last_activity
+                FROM sessions
+                WHERE id = :id
+            SQL, [
+                'id' => $sessionId,
+            ]);
+        } catch (Exception $e) {
+            return '';
+        }
+
 
         if (false === $session) {
             return '';
@@ -61,79 +49,63 @@ class SessionHandler implements SessionHandlerInterface
             : base64_decode($session['data']);
     }
 
-    /**
-     * Write session data
-     *
-     * @param string $sessionId
-     * @param string $data
-     * @return bool
-     */
-    public function write($sessionId, $data): bool
+    public function write(string $sessionId, string $data): bool
     {
-        $this->connection->executeStatement(<<<SQL
-            INSERT INTO sessions (id, data, last_activity)
-            VALUES (:id, :data, :last_activity)
-            ON CONFLICT (id) DO UPDATE
-            SET data = :data, last_activity = :last_activity
-        SQL, [
-            'id'            => $sessionId,
-            'data'          => base64_encode($data),
-            'last_activity' => time(),
-        ]);
+        try {
+            $this->connection->executeStatement(<<<SQL
+                INSERT INTO sessions (id, data, last_activity)
+                VALUES (:id, :data, :last_activity)
+                ON CONFLICT (id) DO UPDATE
+                SET data = :data, last_activity = :last_activity
+            SQL, [
+                'id'            => $sessionId,
+                'data'          => base64_encode($data),
+                'last_activity' => time(),
+            ]);
+        } catch (Exception $e) {
+            return false;
+        }
 
         return true;
     }
 
-    /**
-     * Destroy the session
-     *
-     * @param string $sessionId
-     * @return bool
-     */
-    public function destroy($sessionId): bool
+    public function destroy(string $sessionId): bool
     {
-        $this->connection->executeStatement(<<<SQL
-            DELETE FROM sessions
-            WHERE id = :id
-        SQL, [
-            'id' => $sessionId,
-        ]);
+        try {
+            $this->connection->executeStatement(<<<SQL
+                DELETE FROM sessions
+                WHERE id = :id
+            SQL, [
+                'id' => $sessionId,
+            ]);
+        } catch (Exception $e) {
+            return false;
+        }
 
         return true;
     }
 
-    /**
-     * Clean up old session
-     *
-     * @param int $lifetime
-     * @return int|false Returns the number of deleted sessions on success, or false on failure
-     */
-    public function gc($lifetime)
+    public function gc(int $lifetime): int|false
     {
-        return $this->connection->executeStatement(<<<SQL
-            DELETE FROM sessions
-            WHERE last_activity <= :lifetime
-        SQL, [
-            'lifetime' => time() - $lifetime,
-        ]);
+        try {
+            $rows = $this->connection->executeStatement(<<<SQL
+                DELETE FROM sessions
+                WHERE last_activity <= :lifetime
+            SQL, [
+                'lifetime' => time() - $lifetime,
+            ]);
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return (int) $rows;
     }
 
-    /**
-     * Get the session lifetime
-     *
-     * @return int
-     */
     private function getSessionLifetime(): int
     {
         return (int) ini_get('session.gc_maxlifetime');
     }
 
-    /**
-     * Determine if the session has expired or not
-     *
-     * @param int $lastActivity
-     * @return bool
-     */
     private function hasExpired(int $lastActivity): bool
     {
         return $lastActivity < time() - $this->getSessionLifetime();
